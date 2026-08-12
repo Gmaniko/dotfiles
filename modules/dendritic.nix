b214b2595bbc496867c2592a436baafbb2f88fe8
gmaniko <niko220302@gmail.com>
2026-08-12 21:26:15 +0200

    restructure host configs and add  requirements for bracket syntax.

--- modules/dendritic.nix
+++ modules/dendritic.nix
@@ -1,5 +1,8 @@
-{ inputs, ... }:
+{ den, inputs, ... }:
 {
+
+  _module.args.__findFile = den.lib.__findFile;
+
   imports = [
     (inputs.flake-file.flakeModules.dendritic or { })
     (inputs.den.flakeModules.dendritic or { })

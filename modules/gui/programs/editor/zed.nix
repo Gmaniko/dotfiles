a73ab961a13c89c860b57d96c4756578477ea691
gmaniko <niko220302@gmail.com>
2026-08-15 03:52:11 +0200

    add zed config

---
+++ modules/gui/programs/editor/zed.nix
@@ -0,0 +1,15 @@
+{
+  den.ful.editor.zed.programs.zed-editor = {
+    enable = true;
+    extensions = [ "nix" "python" "typst" ];
+    userSettings = {
+      theme = {
+        mode = "system";
+        dark = "One Dark";
+        light = "One Light";
+      };
+      hour_format = "hour24";
+      vim_mode = true;
+    };
+  };
+}

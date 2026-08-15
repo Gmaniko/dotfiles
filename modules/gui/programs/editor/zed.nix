09442632466142061866ac68bd7639565615ae70
gmaniko <niko220302@gmail.com>
2026-08-15 03:57:59 +0200

    actual fix.

--- modules/gui/programs/editor/zed.nix
+++ modules/gui/programs/editor/zed.nix
@@ -3,11 +3,11 @@
     enable = true;
     extensions = [ "nix" "python" "typst" ];
     userSettings = {
-      theme = {
-        mode = "system";
-        dark = "One Dark";
-        light = "One Light";
-      };
+      # theme = {
+      #   mode = "system";
+      #   dark = "One Dark";
+      #   light = "One Light";
+      # };
       hour_format = "hour24";
       vim_mode = true;
     };
